d444ba51ed35bf3d4f4fe93a193bc639b7051600
DMG <daniel.marce@mataro.epiaedu.cat>
2025-03-27 20:04:17 +0100

    feat: rewrite status class assignment in ItemCard component

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,33 +1,26 @@
-import styles from "./ItemCard.module.css"
+import styles from "./ItemCard.module.css";
 
 export const ItemCard = ({ item }) => {
   const { title, description, status } = item;
 
-  const titleClass =
-    status === "done"
-      ? `${styles["item__title"]} ${styles["item__title--strikethrough"]}`
-      : status === "in progress"
-      ? `${styles["item__title"]} ${styles["item__title--bold"]}`
-      : status === "pending"
-      ? `${styles["item__title"]} ${styles["item__title--regular"]}`
-      : "";
+  const titleClass = {
+    pending: `${styles["item__title"]} ${styles["item__title--regular"]}`,
+    "in progress": `${styles["item__title"]} ${styles["item__title--bold"]}`,
+    done: `${styles["item__title"]} ${styles["item__title--strikethrough"]}`,
+  };
 
-  const statusClass =
-    status === "done"
-      ? `${styles["item__status"]} ${styles["item__status--green"]}`
-      : status === "in progress"
-      ? `${styles["item__status"]} ${styles["item__status--blue"]}`
-      : status === "pending"
-      ? `${styles["item__status"]} ${styles["item__status--grey"]}`
-      : "";
-
+  const statusClass = {
+    pending: `${styles["item__status"]} ${styles["item__status--green"]}`,
+    "in progress": `${styles["item__status"]} ${styles["item__status--blue"]}`,
+    done: `${styles["item__status"]} ${styles["item__status--grey"]}`,
+  };
 
   return (
     <>
       <li className={styles["itemCard__wrapper"]}>
-        <h4 className={titleClass}>{title}</h4>
+        <h4 className={titleClass[status]}>{title}</h4>
         <p className={styles["item__description"]}>{description}</p>
-        <span className={statusClass}>{status}</span>
+        <span className={statusClass[status]}>{status}</span>
       </li>
     </>
   );
